249e68f7a7ffa9099ff2282d43e631850bac4f83
maick7777777 <mi.jaque@duocuc.cl>
2026-05-29 23:42:31 -0400

    se creo hastas la opcion 3 revisar

--- EjercicioFunciones/EjerciciosPreparativo/ejercicio.py
+++ EjercicioFunciones/EjerciciosPreparativo/ejercicio.py
@@ -1,18 +1,17 @@
-producto ={
+productos ={
     "Mouse" : [10, 15000],
     "Teclado" : [5, 25000],
     "Monitor" : [3, 18000]
 
-
-
 }
 
+#funcion para agregar productos
 def agregar_productos(productos):
     nombre=input("Nombre del producto: ").strip()#quita los espacios en blanco 
     if nombre== " ":
         print("el nombre no puede estar vacio")
 
-    if nombre in producto:
+    if nombre in productos:
         print("el nombre ya esta en productos ")
 
     stock=int(input("Ingrese stock-->"))
@@ -20,6 +19,27 @@ def agregar_productos(productos):
 
     productos[nombre] = [stock,precio]
     print("Productos agregados correctamente ")
+
+#funcion para mostrar los productos
+"""""
+def mostrar_producto(productos):
+    for clave , valor in productos.items():
+        print(f"nombre prodructo {clave}|stock y valor {valor}")
+"""""
+def mostrar_producto(productos):
+    for i in productos.items():
+        print(i)
+
+#funcion para buscar los productos
+def buscar_productos(productos):
+    buscador=input("ingrese el producto a buscar-->").capitalize()
+    for i in productos:
+        if buscador in i:
+            print(f"producto {buscador} existente")
+            break
+        else:
+            print("el producto no existe")  
+            break  
 
 while True:
     print("MENU REGISTRO")
@@ -31,11 +51,11 @@ while True:
     op =int(input("Ingrese la opcion-->"))
 
     if op==1:
-        print
+        agregar_productos(productos)
     elif op==2:
-        print
+        mostrar_producto(productos)
     elif op==3:
-        print  
+        buscar_productos(productos)
     elif op==4:
         print
     elif op==5:
